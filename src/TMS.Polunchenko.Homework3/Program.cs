25053bc624c7c370a3643ce5fd349471a7470d7d
tnerozzik <91133609+tnerozzik@users.noreply.github.com>
2021-10-07 18:49:02 +0300

    add solution 1 of homework3

---
+++ src/TMS.Polunchenko.Homework3/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace TMS.Polunchenko.Homework3
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //var backlog = new Todo[100];
+            var backlog = new List<Todo>();
+            var inputStop = false;
+
+            InputBacklogList(backlog, inputStop);
+            ShowBacklog(backlog);
+            ChangeBacklogTodoItemsStatus(backlog, inputStop);
+            ShowBacklog(backlog);
+
+            Console.ReadLine();
+        }
+
+        private static void InputBacklogList(List<Todo> backlog, bool inputStop)
+        {
+            while (!inputStop)
+            {
+                Console.Write("Enter name: ");
+                var name = Console.ReadLine();
+                Console.Write("Enter description: ");
+                var description = Console.ReadLine();
+
+                var todo = new Todo
+                {
+                    Name = name,
+                    Description = description,
+                };
+
+                //backlog[0] = todo;
+                backlog.Add(todo);
+
+                inputStop = InputStop();
+                Console.WriteLine();
+            }
+        }
+
+        private static void ChangeBacklogTodoItemsStatus(List<Todo> backlog, bool inputStop)
+        {
+            while (!inputStop)
+            {
+                Console.Write("Please enter todo Id: ");
+                var userInput = Console.ReadLine();
+
+                foreach (var todo in backlog)
+                {
+                    if (todo.GetId() == userInput.ToUpperInvariant())
+                    {
+                        Console.WriteLine("Todo found! Enter new todo status..");
+                        Console.WriteLine("Availiable statuses: InProgress, Done, Canceled.");
+                        Console.Write("Enter status: ");
+                        var newStatus = Console.ReadLine();
+                        todo.SetStatus(todo.ConvertStatus(newStatus));
+                    }
+                }
+
+                inputStop = InputStop();
+                Console.WriteLine();
+            }
+        }
+
+        private static void ShowBacklog(List<Todo> backlog)
+        {
+            Console.WriteLine("=======\n");
+            foreach (var todo in backlog)
+            {
+                todo.GetInfo();
+            }
+            Console.WriteLine("=======\n");
+        }
+
+        private static bool InputStop()
+        {
+            Console.Write("Stop? (Press Y/y): ");
+            var key = Console.ReadKey().Key;
+            Console.WriteLine();
+            return key == ConsoleKey.Y;
+        }
+    }
+}
---
+++ src/TMS.Polunchenko.Homework3/Todo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Polunchenko.Homework3
+{
+    class Todo
+    {
+        private readonly string Id = Guid.NewGuid().ToString().ToUpper().Substring(0, 5);
+        private readonly DateTime _dateTime = DateTime.Now;
+        private TodoStatus _status = TodoStatus.Backlog;
+
+        
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+       
+        public void SetStatus(TodoStatus status)
+        {
+            _status = status;
+        }
+
+       
+        public string GetId()
+        {
+            return Id;
+        }
+
+       
+        public void GetInfo()
+        {
+            Console.WriteLine(Id);
+            Console.WriteLine("---");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Status: {_status}");
+            Console.WriteLine($"DateTime: {_dateTime}");
+            Console.WriteLine();
+        }
+
+        public TodoStatus ConvertStatus(string status)
+        {
+            return status switch
+            {
+                "Backlog" => TodoStatus.Backlog,
+                "InProgress" => TodoStatus.InProgress,
+                "Done" => TodoStatus.Done,
+                "Canceled" => TodoStatus.Canceled,
+                _ => TodoStatus.Unknown,
+            };
+        }
+    }
+}
+
