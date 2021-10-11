65721533edf5ed53d1e393a579e7c6ea7b0eb707
tnerozzik <91133609+tnerozzik@users.noreply.github.com>
2021-10-11 19:01:41 +0300

    add solution homework 4

---
+++ src/TMS.Polunchenko.Homework4/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Polunchenko.Homework4
+{   /// <summary>
+    /// absract class
+    /// </summary>
+    public abstract class Animal
+    {
+        public char Sex { get; set; }
+        public int Age { get; set; }
+        public string PlaceofBirth { get; set; }
+
+        public int AviaryNumber { get; set; }
+    }
+}
---
+++ src/TMS.Polunchenko.Homework4/Birds.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Polunchenko.Homework4
+{
+    public class Birds:Animal,IClinning
+    {  
+       public List <BirdsSp> Birdss { get; set; }
+        public void Cliner()
+        {
+            Console.WriteLine("Вольеры птиц на уборке");
+        }
+    }
+}
