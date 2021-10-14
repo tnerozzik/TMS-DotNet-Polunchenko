b55b5fa23c5e115539d8bdfc42a20f398f9377bb
tnerozzik <91133609+tnerozzik@users.noreply.github.com>
2021-10-14 17:51:18 +0300

    add solution for homework-5

---
+++ src/TMS.Polunchenko.Homework5/Bankomat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Polunchenko.Homework5
+{
+    class Bankomat
+    {
+        int sum;
+        public event Action<string> Notify;
+
+
+        public int Sum
+        { 
+            get
+            {
+                return sum;
+            }
+        }
+
+        public void  Enrollment (int _sum)
+        {
+            sum +=_sum;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Notify?.Invoke($"Успешно!\nПополнение счета {_sum}руб");
+
+        }
+        public void Writting (int _sum)
+        {   if (_sum > sum)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Notify?.Invoke($"Ошибка!\nНедостаточно средств");
+            }
+            else
+            {
+                sum = sum - _sum;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Успешно!\nЗаберите Ваши деньги\n -{_sum}руб на счету"); 
+            }
+
+        }
+
+
+    }
+}
