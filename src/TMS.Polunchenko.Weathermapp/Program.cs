c1a261d0357d2bddfda84deef510277f2fa277c8
tnerozzik <91133609+tnerozzik@users.noreply.github.com>
2021-11-17 22:30:16 +0300

    app API for weather forecast

---
+++ src/TMS.Polunchenko.Weathermapp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace TMS.Polunchenko.Weathermapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start start = new Start();
+            start.CreateResponce();
+            start.AdvertisementNews();
+
+
+
+        }
+
+    }
+    
+ }
---
+++ src/TMS.Polunchenko.Weathermapp/Wether.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Polunchenko.Weathermapp
+{
+
+    public class WetherMenu
+    {
+        public Main main { get; set; }
+        public string Name { get; set; }
+        public List <Weather> weather { get; set; }
+        public Wind wind { get; set; }
+
+    }
+
+    public class Main
+    {
+        public double Temp { get; set; }
+        public double Feels_like { get; set; }
+        public double Humidity { get; set; }
+    }
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string Description { get; set; }
+        public string icon { get; set; }
+
+    }
+    public class Wind
+    {
+        public double Speed { get; set; }
+    }
+
+    public class NewsMenu
+    {
+
+        //public string Status { get; set; }
+        public List<Article> Articles { get; set; }
+
+
+    }
+    public class Article
+    {
+        public Source Source { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string UrlToImage { get; set; }
+        public string PublishedAt { get; set; }
+        public string Content { get; set; }
+
+    }
+    public class Source
+    {
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
